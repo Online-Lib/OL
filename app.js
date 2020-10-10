8618b25c1b279a0bec4645d7ab8c82b6978a8a81
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-10 05:59:47 +0530

    setup ejs

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'Online Library')))
+app.get('/ping', (req, res, next)=> {
+	res.render('ping', {title:'Pong!'})
+})
 
 module.exports = app
