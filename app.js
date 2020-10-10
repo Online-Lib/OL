0970fe5daffefb17386d667c9908836c88df9c74
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-10 06:09:08 +0530

    add public folder with for css files

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'Online Library')))
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/ping', (req, res, next)=> {
 	res.render('ping', {title:'Pong!'})
 })
