ef5c244b22c89a6a8e3b74a7a9b3744f7c6f2996
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-11 14:10:46 +0530

    fetch search results from api

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,17 @@
-exports.getSearch = (req, res ,next )=> {
-	res.render('search', {title: 'Search results'})
-}
+const gutendex = require("../api/gutendex");
+
+exports.getSearch = async (req, res, next) => {
+  console.log(req.query);
+  try {
+    const response = await gutendex.get("/books", {
+      params: {
+        search: req.query.query,
+      },
+    });
+
+    const searchResults = response.data;
+    res.render("search", { title: "Search results", searchResults });
+  } catch (error) {
+    console.log(error);
+  }
+};
