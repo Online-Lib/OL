16b0884f4daaeaf5878fba79ac51a01997c369d6
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2020-10-12 16:58:25 +0200

    Add google oauth2
    + Add google oauth2 passport
    + Update readme

--- security/passport.js
+++ security/passport.js
@@ -2,7 +2,8 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local');
 const { comparePassword } = require('../helpers/bcrypt');
 const User = require('../models/User')
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = require('../configs/env.config')
 /**
  * Use local stategy with username and password
  */
@@ -18,6 +19,46 @@ passport.use(new LocalStrategy(
             //Compare hash password and password
             if (!comparePassword(password, user.password)) { return done(null, false); }
             return done(null, user);
+        });
+    }
+));
+
+/**
+ * Google oauth2 strategy
+ */
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+    //For google +
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOne({
+            googleId: profile.id
+        }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            //console.log(profile)
+            //No user was found... so create a new user with values from Facebook (all the profile. stuff)
+            if (!user) {
+                user = new User({
+                    name: profile.displayName,
+                    //email: profile.emails[0].value,
+                    username: profile.username,
+                    provider: profile.provider,
+                    //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
+                    googleId: profile.id
+                });
+                user.save(function (err) {
+                    if (err) console.log(err);
+                    return cb(err, user);
+                });
+            } else {
+                //found user. Return
+                return cb(err, user);
+            }
         });
     }
 ));
